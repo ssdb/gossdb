65b123c2ad686a4ca6ce9b1841108519a5695068
Brian McQueen <bmcqueen@walmartlabs.com>
2015-01-18 16:45:45 -0800

    change 2 more == 2 to > 0

--- ssdb/ssdb.go
+++ ssdb/ssdb.go
@@ -107,7 +107,7 @@ func (c *Client) Get(key string) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	if len(resp) == 2 && resp[0] == "ok" {
+	if len(resp) > 0 && resp[0] == "ok" {
 		return resp[1], nil
 	}
 	if resp[0] == "not_found" {
@@ -131,7 +131,7 @@ func (c *Client) Del(key string) (interface{}, error) {
 	}
 
 	//response looks like this: [ok 1]
-	if len(resp) == 2 && resp[0] == "ok" {
+	if len(resp) > 0 && resp[0] == "ok" {
 		return true, nil
 	}
 	return nil, fmt.Errorf("bad response:resp:%v:", resp)
