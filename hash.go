b4e43bfefd03ca61b3f78fa0b2b8d8b5266dd1c6
zhangjunli <1217105106@qq.com>
2020-03-21 16:52:01 +0800

    add hash.go

---
+++ hash.go
@@ -0,0 +1,114 @@
+package gossdb
+
+import "strconv"
+
+//:hclear
+func (c *Client) Hclear(key string) error {
+	_, err := c.Do("hclear", key)
+	return err
+}
+
+//:hget
+func (c *Client) Hget(key string, filed string) (string, error) {
+	result, err := c.Do("hget", key, filed)
+	if err != nil {
+		return "", err
+	}
+	return result[0], nil
+}
+
+//:multi_hget
+func (c *Client) Hmget(key string, m map[string]interface{}) ([]string, error) {
+	args := []interface{}{}
+	for filed, val := range m {
+		args = append(args, filed)
+		args = append(args, val)
+	}
+	result, err := c.Do("multi_hget", key, args)
+	if err != nil {
+		return []string{}, err
+	}
+	return result, nil
+}
+
+//:hset
+func (c *Client) Hset(key string, filed string, val interface{}) error {
+	_, err := c.Do("hset", key, filed, val)
+	return err
+}
+
+//:multi_hset
+func (c *Client) HMset(key string, filed string, val interface{}) error {
+	_, err := c.Do("multi_hset", key, filed, val)
+	return err
+}
+
+//:hdel
+func (c *Client) Hdel(key string, filed string) error {
+	_, err := c.Do("hdel", key, filed)
+	return err
+}
+
+//:multi_hdel
+func (c *Client) Hmdel(key string, filed ...string) error {
+	_, err := c.Do("multi_hdel", key, filed)
+	return err
+}
+
+//:hincr
+func (c *Client) Hincr(key string, filed string, increment int64) (int64, error) {
+	reslut, err := c.Do("hincr", key, filed, increment)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(reslut[0], 10, 64)
+}
+
+//:hsize
+func (c *Client) Hsize(key string) (int64, error) {
+	reslut, err := c.Do("hsize", key)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(reslut[0], 10, 64)
+}
+
+//:hexists
+func (c *Client) Hexists(key string, filed string) (bool, error) {
+	reslut, err := c.Do("hexists", key, filed)
+	if err != nil {
+		return false, err
+	}
+	exit := true
+	if reslut[0] == "0" {
+		exit = false
+	}
+	return exit, nil
+}
+
+//:hdecr
+func (c *Client) Hdecr(key string, filed string, decrement int64) (int64, error) {
+	reslut, err := c.Do("hdecr", key, filed, decrement)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(reslut[0], 10, 64)
+}
+
+//:hkeys
+func (c *Client) Hkeys(key string) ([]string, error) {
+	reslut, err := c.Do("hkeys", key)
+	if err != nil {
+		return []string{}, err
+	}
+	return reslut, nil
+}
+
+//:hscan
+func (c *Client) Hscan(key string) ([]string, error) {
+	reslut, err := c.Do("hscan", key)
+	if err != nil {
+		return []string{}, err
+	}
+	return reslut, nil
+}
--- queue.go
+++ queue.go
@@ -1,14 +1,12 @@
 package gossdb
 
+import (
+	"strconv"
+)
+
 //:qpush_front
 func (c *Client) QpushFront(key string, val interface{}) error {
 	_, err := c.Do("qpush_front", key, val)
-	return err
-}
-
-//:qset
-func (c *Client) Qset(key string, index int64, val interface{}) error {
-	_, err := c.Do("qset", key, index, val)
 	return err
 }
 
@@ -21,6 +19,27 @@ func (c *Client) QpopFront(key string) (string, error) {
 	return result[0], nil
 }
 
+//:qpush_back
+func (c *Client) Qpushback(key string, val interface{}) error {
+	_, err := c.Do("qpush_back", key, val)
+	return err
+}
+
+//:qpop_back
+func (c *Client) Qpopback(key string) (string, error) {
+	result, err := c.Do("qpop_back", key)
+	if err != nil {
+		return "", err
+	}
+	return result[0], nil
+}
+
+//:qset
+func (c *Client) Qset(key string, index int64, val interface{}) error {
+	_, err := c.Do("qset", key, index, val)
+	return err
+}
+
 //:qget
 func (c *Client) Qget(key string, index int64) (string, error) {
 	result, err := c.Do("qget", key, index)
@@ -29,3 +48,40 @@ func (c *Client) Qget(key string, index int64) (string, error) {
 	}
 	return result[0], nil
 }
+
+//:qsize
+func (c *Client) Qsize(key string) (int64, error) {
+	result, err := c.Do("qsize", key)
+	if err != nil {
+		return 0, err
+	}
+	size, err := strconv.ParseInt(result[0], 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return size, nil
+}
+
+//:qclear
+func (c *Client) Qclear(key string) error {
+	_, err := c.Do("qclear", key)
+	return err
+}
+
+//:qslice
+func (c *Client) Qslice(key string, start, end int64) ([]string, error) {
+	result, err := c.Do("qslice", key, start, end)
+	if err != nil {
+		return []string{}, err
+	}
+	return result, nil
+}
+
+//:qlist (for queue/list type only)
+//func (c *Client) Qlist() ([]string, error) {
+//	result, err := c.Do("qlist")
+//	if err != nil {
+//		return []string{}, err
+//	}
+//	return result, nil
+//}
